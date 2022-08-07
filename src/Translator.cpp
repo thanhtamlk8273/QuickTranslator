04cfd795f74b8055d073a2eed8ad1ecca76b63cd
Tran Thanh Tam <thanhtamlk8273@gmail.com>
2022-08-07 14:15:20 +0700

    another small improvement

--- src/Translator.cpp
+++ src/Translator.cpp
@@ -13,6 +13,7 @@
 #include <map>
 #include <iostream>
 #include <algorithm>
+#include <array>
 
 namespace {
     static const std::map<UChar32, UChar32> punctationTable = {
@@ -149,16 +150,16 @@ std::vector<std::pair<icu::UnicodeString, icu::UnicodeString>> Translator::Trans
     std::set<int> vps_possible_lengths = vp_dic->getLengthList();
     std::vector<std::pair<icu::UnicodeString, icu::UnicodeString>> result;
     int i = 0;
-    int32_t s_length = s.countChar32();
+    int32_t s_length = s.length();
     while (i < s_length)
     {
-        int tmp_end = (vp_dic->getMaxLength() < (s_length - i)) ? (i + vp_dic->getMaxLength()) : s_length;
+        int max_length = (vp_dic->getMaxLength() < (s_length - i)) ? (i + vp_dic->getMaxLength()) : s_length;
         /* Update possible lengths lists */
-        updatePossibleLengthList(names_possible_lengths, tmp_end);
-        updatePossibleLengthList(vps_possible_lengths, tmp_end);
+        updatePossibleLengthList(names_possible_lengths, max_length);
+        updatePossibleLengthList(vps_possible_lengths, max_length);
         /* prepare possible substrings */
-        std::vector<icu::UnicodeString> subcn_vec;
-        for(int j = 1; j < tmp_end + 1; ++j)
+        std::vector<icu::UnicodeString> subcn_vec(max_length);
+        for(int j = 1; j < max_length + 1; ++j)
         {
             subcn_vec.push_back(s.tempSubString(i, j));
 
@@ -205,7 +206,7 @@ std::vector<std::pair<icu::UnicodeString, icu::UnicodeString>> Translator::Trans
         /* No substring matches? */
         if (notTranslated)
         {
-            UChar c = s.charAt(i);
+            UChar c = s.char32At(i);
             if(u_ispunct(c))
             {
                 /* With punctation, we will find its equivalent in punctationTable
@@ -221,9 +222,9 @@ std::vector<std::pair<icu::UnicodeString, icu::UnicodeString>> Translator::Trans
                     result.emplace_back(getPunc(c), c);
                 }
             }
-            if (ublock_getCode(c) == UBLOCK_BASIC_LATIN)
+            else if (ublock_getCode(c) == UBLOCK_BASIC_LATIN)
             {
-                icu::UnicodeString latinString = getLatinString(s.tempSubString(i, tmp_end));
+                icu::UnicodeString latinString = getLatinString(s.tempSubString(i, max_length));
                 result.emplace_back(latinString, latinString);
                 step = latinString.length();
             }
